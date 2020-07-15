4b79cbda7d5e1b8b0d13309d1f9caa02e2b1d1c9
Wouter van Veen <wgvanveen@gmail.com>
2020-07-15 16:31:21 +0200

    Added auto branching feature

--- science_data_structure/structures.py
+++ science_data_structure/structures.py
@@ -27,11 +27,15 @@ class Leaf(Node):
     def __init__(self,
                  parent_path: Path,
                  name: str,
-                 content: Dict[str, Node]) -> None:
+                 content: Dict[str, Node],
+                 enable_auto_branching: bool = True,
+                 overwrite: bool = False) -> None:
         self._parent_path = parent_path  # type: Path
         self._name = name  # type: str
         self._content = content  # type: Dict[str, Node] 
-
+        self._enable_auto_branching = enable_auto_branching
+        self._overwrite = overwrite
+        self._kill = []  # type: List[Node]
 
     @property
     def name(self) -> str:
@@ -47,32 +51,33 @@ class Leaf(Node):
             self._content[node_name].write()
 
     def add_leaf(self,
-                 name: str, 
-                 exist_ok=False) -> None:
-        if name not in self._content or exist_ok:
-            self._content[name] = Leaf(self.path,
-                                       name,
-                                       {})
-    
+                 name: str) -> "Leaf":
+        if name not in self._content or self._overwrite:
+            leaf = Leaf(self.path,
+                        name,
+                        {})
+            self._content[name] = leaf
+            return  leaf
+        raise FileExistsError
+
     def add_data(self,
                  name: str,
-                 data: numpy.ndarray, 
-                 exist_ok=False):
-        if name not in self._content or exist_ok:
+                 data: numpy.ndarray):
+        if name not in self._content or self._overwrite:
             data_node = Data(self.path, name)
             data_node.data = data
             self._content[name] = data_node
         return self._content[name]
-    
+
     def read(self) -> "Leaf":
         files = list(self.path.glob("*"))
-        
+
         # leafs
         leafs = filter(lambda x: x.is_dir(), files)
-        
+
         # data
         data = filter(lambda x: x.suffix == ".npy", files)
-        
+
         for leaf in leafs:
             self._content[leaf.name] = Leaf(self.path, 
                                             leaf.name,
@@ -83,10 +88,126 @@ class Leaf(Node):
             self._content[data_node.stem] = Data(self.path,
                                             data_node.stem)
     def __getitem__(self, name: str) -> Node:
-        return self._content[name]
+        try:
+            return self._content[name]
+        except KeyError:
+            # the variable does not exist
+            if self._enable_auto_branching:
+                # create a new leaf based on the name provided
+                return self.add_leaf(name)
+
+        raise KeyError
+
+    def __setitem__(self, key: str, item) -> None:
+        """
+        TODO check if the key is of value string
+        TODO handle setting leaf or node with None
+        TODO handle the deletion of the node when it is overwritten by another node
+        """
+        if not isinstance(item, Node):
+            if isinstance(item, numpy.ndarray):
+                if self._enable_auto_branching and key not in self._content:
+                    self._content[key] = Data(self.path,
+                                              key)
+                elif key in self._content and self._overwrite:
+                    self._kill += [self._content[key]]
+                    self._content[key] = Data(self.path,
+                                              key)
+                elif key not in self._content:
+                    raise PermissionError
+                else:
+                    raise FileExistsError
+        else:
+            if self._enable_auto_branching and key not in self._content:
+                self._content[key] = Leaf(self.path,
+                                          key)
+            elif key in self._content and self._overwrite:
+                self._kill += [self._content[key]]
+                self._content[key] = Leaf(self.path,
+                                          key)
+            elif key not in self._content:
+                raise PermissionError
+            else:
+                raise FileExistsError
+
+    @property
+    def enable_auto_branching(self) -> bool:
+        return self._enable_auto_branching
+
+    @enable_auto_branching.setter
+    def enable_auto_branching(self, enable_auto_branching) -> None:
+        self._enable_auto_branching = enable_auto_branching
+
+        for key in self._content.keys():
+            if isinstance(self._content[key], Leaf):
+                self._content[key].enable_auto_branching = enable_auto_branching
+
+    @property
+    def overwrite(self) -> bool:
+        return self._overwrite
+
+    @overwrite.setter
+    def overwrite(self, overwrite: bool) -> None:
+        self._overwrite = overwrite
+        for key in self._content.keys():
+            if isinstance(self._content[key], Leaf):
+                self._content[key].overwrite = overwrite
+
 
     def keys(self) -> List[str]:
         return list(self._content.keys())
+
+
+class StructuredDataset(Leaf):
+    """
+    StructuredDataset the base class of Leaf
+    """
+    def __init__(self,
+                 path: Path,
+                 name: str,
+                 content: Dict[str, Node],
+                 enable_auto_branching: bool = True,
+                 overwrite: bool = False) -> None:
+        super().__init__(path , "{:s}.struct".format(name),
+                         content,
+                         enable_auto_branching = enable_auto_branching,
+                         overwrite = overwrite)
+
+    def write(self, 
+              exist_ok: bool =False) -> None:
+        if self.path.exists and not exist_ok:
+            raise FileExistsError
+        self.path.mkdir(exist_ok=True)
+    
+        for node_name in self._content.keys():
+            self._content[node_name].write()
+
+    @staticmethod
+    def read(path: Path) -> "StructuredDataset":
+        # load all the files
+        files = list(path.glob("*"))
+
+        # leafs
+        leafs = filter(lambda x: x.is_dir(), files)
+        
+        # data
+        data = filter(lambda x: x.suffix  == ".npy", files)
+        
+        content = {}
+        for leaf in leafs:
+            content[leaf.name] = Leaf(path, 
+                                      leaf.name,
+                                      {})
+            content[leaf.name].read()
+
+        for data_node in data:
+            content[data_node.stem] = Data(path,
+                                           data_node.stem)
+
+        return StructuredDataset(path.parent, path.stem, content)
+
+
+
 
 class Data(Node):
 
@@ -133,68 +254,4 @@ class Data(Node):
             self._data = numpy.load(self.path)
         return self._data
 
-class StructuredDataset:
-    """
-    StructuredDataset
-    """
-    def __init__(self,
-                 path: Path,
-                 name: str,
-                 content: Dict[str, Node]) -> None:
-        self._path = path  # type: Path
-        self._content = content  # type: Dict[str, Node]
-        self._name = name  # type: str
 
-    @property
-    def path(self) -> Path:
-        return self._path / "{:s}.struct".format(self._name)
-
-    
-    def add_leaf(self, 
-                 name: str, 
-                 exist_ok=False) -> None:
-        if name not in self._content or exist_ok:
-            leaf = Leaf(self.path,
-                                       name,
-                                       {})
-            self._content[name] = leaf
-            return leaf
-
-    def write(self, 
-              exist_ok: bool =False) -> None:
-        if self.path.exists and not exist_ok:
-            raise Exception
-        self.path.mkdir(exist_ok=True)
-    
-        for node_name in self._content.keys():
-            self._content[node_name].write()
-
-    @staticmethod
-    def read(path: Path) -> "StructuredDataset":
-        # load all the files
-        files = list(path.glob("*"))
-
-        # leafs
-        leafs = filter(lambda x: x.is_dir(), files)
-        
-        # data
-        data = filter(lambda x: x.suffix  == ".npy", files)
-        
-        content = {}
-        for leaf in leafs:
-            content[leaf.name] = Leaf(path, 
-                                      leaf.name,
-                                      {})
-            content[leaf.name].read()
-
-        for data_node in data:
-            content[data_node.stem] = Data(path,
-                                           data_node.stem)
-
-        return StructuredDataset(path.parent, path.stem, content)
-
-    def __getitem__(self, name: str) -> Node:
-        return self._content[name]
-
-    def keys(self) -> List[str]:
-        return list(self._content.keys())
--- science_data_structure/test_structures.py
+++ science_data_structure/test_structures.py
@@ -10,17 +10,19 @@ class TestStructuredDataset(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+        self._test_path = pathlib.Path("/home/wgvanveen/Desktop/test")
+        self._test_path.mkdir(exist_ok=True)
 
     def test_writing(self):
         leaf_name = "leaf_1"
         # create an empty data-set
         dataset = structures.StructuredDataset(pathlib.Path("/home/wgvanveen/Desktop"),
                                                "test_set",
-                                               {})
+                                               {},
+                                               overwrite=True)
         dataset.write(exist_ok=True)
 
-        # write the data-set
+        # write the data-set 
         self.assertTrue(dataset.path.exists())
 
         # check if it will overwrite, if not permitted
@@ -36,8 +38,8 @@ class TestStructuredDataset(unittest.TestCase):
         x = numpy.linspace(0, 10, 1000)
         y = x ** 2
 
-        data_x = leaf.add_data("x", x, exist_ok=True)
-        data_y = leaf.add_data("y", y, exist_ok=True)
+        data_x = leaf.add_data("x", x)
+        data_y = leaf.add_data("y", y)
 
         dataset.write(exist_ok=True)
 
@@ -49,7 +51,8 @@ class TestStructuredDataset(unittest.TestCase):
         # create an empty data-set
         dataset = structures.StructuredDataset(pathlib.Path("/home/wgvanveen/Desktop"),
                                                "test_set",
-                                               {})
+                                               {},
+                                               overwrite=False)
 
         dataset.add_leaf(leaf_name)
         # try to add a leaf that already exists
@@ -57,12 +60,64 @@ class TestStructuredDataset(unittest.TestCase):
             dataset.add_leaf(leaf_name)
 
         # add duplicate, overwriting the original leaf
+        dataset.overwrite = True
         try:
-            dataset.add_leaf(leaf_name, exist_ok=True)
+            dataset.add_leaf(leaf_name)
         except FileExistsError:
             self.fail("dataset.add_leaf raised a FileExistsError")
 
+    def test_auto_branching(self) -> None:
+        data_set = structures.StructuredDataset(self._test_path,
+                                                "test_auto_branching",
+                                                {},
+                                                enable_auto_branching=True)
+        leaf_name_1 = "auto_branched"
+        leaf_name_2 = "not_auto_branched"
+        try:
+            leaf = data_set[leaf_name_1]
+            self.assertNotEqual(leaf, None)
+
+            data_set.write(exist_ok=True)
+            self.assertTrue(leaf.path.exists())
+        except KeyError:
+            self.fail("Did not auto-branch")
+
+
+        # now disable autobranching
+        data_set.enable_auto_branching = False
+        with self.assertRaises(KeyError):
+            data_set[leaf_name_2]
+        data_set.write(exist_ok=True)
+        # check if the second leaf exists
+        self.assertFalse((data_set.path / leaf_name_2).exists())
+
+        # test auto branching with variables
+        data_set.enable_auto_branching = True
+        x = numpy.linspace(0, 100, 1000)
+        data_set[leaf_name_1]["x"] = x
+        try:
+            data_set[leaf_name_1]["x"]
+
+        except KeyError:
+            self.fail("auto branching failed")
+
+        # try to overwrite the value in x
+        with self.assertRaises(FileExistsError):
+            data_set[leaf_name_1]["x"] = x
+
+
+        # disable auto branching
+        data_set.enable_auto_branching = False
+        with self.assertRaises(PermissionError):
+            data_set[leaf_name_1]["x_2"] = x
+
+        with self.assertRaises(KeyError):
+            data_set[leaf_name_1]["x_2"]
+
+
     def tearDown(self):
+        # clean up the test environment
+        # TODO First need to write the delete function (again)
         pass
 
 if __name__ == "__main__":
